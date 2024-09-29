7f12a45f51f13e7aa0932da4511deacf797ddd76
PedrohAmancio <pedro.h.rosa10@aluno.senai.br>
2024-09-29 14:40:37 -0300

    Add files via upload

--- LISTA_REVISAO10_PEDRO.java
+++ LISTA_REVISAO10_PEDRO.java
@@ -15,8 +15,7 @@ public class LISTA_REVISAO10_PEDRO {
          Scanner ler = new Scanner(System.in);
 
         // Leitura do valor de N (entre 0 e 50)
-        int N=0; int leitura[] = new int [N];
-        int neg[] = new int [N];int pos[] = new int [N];
+        int N=0;
         int valor_V_N = 0;int valor_V_P=0;
         
         System.out.print("Digite um número N (entre 0 e 50): ");
@@ -31,29 +30,35 @@ public class LISTA_REVISAO10_PEDRO {
             }
         }
         }
+         int leitura[] = new int [N];
+        int neg[] = new int [N];int pos[] = new int [N];
         for(int i = 0; i < N; ++i){
         System.out.println("Digite numeros");
         leitura[i]=ler.nextInt();
         }
         for(int i =0; i < N; ++i){
              if(leitura[i] >= 0){
-           leitura[i]= pos[i];
+           pos[i]=leitura[i];
            ++valor_V_P;
         }
         else if(leitura[i]<0){
-            leitura[i]= neg[i];
+            neg[i]=leitura[i];
             ++valor_V_N;
         }
         else{
             System.out.println("Recomece o codigo");
         }
         }
-        for(int i = 0; i < 1; ++i){
-            System.out.println("Esses sao os valores positivos "+ pos[i]);
-            System.out.println("Esses sao os valores negativos"+neg[i]);
-            System.out.println("Esses sao os valores contidos(vetor de negativos)" + valor_V_N);
-            System.out.println("Esses sao os valores contidos(vetor de positivos) " + valor_V_P);
+        System.out.print("Esses sao os valores positivos \n");
+        for(int i = 0; i < N; ++i){
+             System.out.print(pos[i] + ",");
         }
+          System.out.println("Esses sao os valores negativos ");
+          for(int i = 0; i < N; ++i){
+            System.out.print(neg[i] + ",");
+        }
+        System.out.println("Esses sao os valores contidos(vetor de negativos)" + valor_V_N);
+            System.out.println("Esses sao os valores contidos(vetor de positivos) " + valor_V_P);
         }
 }
 
