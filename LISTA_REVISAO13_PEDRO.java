9d24516c2922ad45073fb50ad540809bc321b058
PedrohAmancio <pedro.h.rosa10@aluno.senai.br>
2024-09-29 11:50:48 -0300

    Add files via upload

---
+++ LISTA_REVISAO13_PEDRO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lista_revisao13_pedro;
+import java.util.Scanner;
+/**
+ *
+ * @author farne
+ */
+public class LISTA_REVISAO13_PEDRO {
+
+    public static void main(String[] args) {
+       Scanner ler = new Scanner(System.in);
+       
+       for(int i=150; i <= 300; ++i){
+           System.out.print(i+"\n");
+       }
+    }
+}
